ee935f76a8fc2d0e28cab84eb2f7149c5e0c6908
zll18314831310 <18314831310@163.com>
2018-07-21 23:47:45 +0800

    koa router 路由

---
+++ koa2/demo5.js
@@ -0,0 +1,17 @@
+const Koa = require('koa')
+const app = new Koa()
+const Router = require('koa-router')
+
+const router = new Router()
+router.get('/',(ctx, next)=> {
+  ctx.body = '<h1>hello zhaolele</h1>'
+})
+router.get('/todo',(ctx,next)=> {
+  ctx.body = '<h2>Todo Page</h2>'
+})
+app.use(router.routes())
+.use(router.allowedMethods())
+
+app.listen(3000,()=>{
+  console.log('app server start success!')
+})
