9ae7173ac4e82d81f18007b521a30f2dd92f175f
zll18314831310 <18314831310@163.com>
2018-08-02 17:18:52 +0800

    react-umi框架开发饿了么页面

---
+++ umi_app/dist/static/common-umi.9ed38b1a.async.js
@@ -0,0 +1,1 @@
+webpackJsonp([0],{"9+6m":function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var u=t("tRWo"),a=t.n(u);n.default=function(){return a.a.createElement("div",null,"Index Page")}}});
---
+++ umi_app/readme.md
@@ -0,0 +1,3 @@
+1. umi极快，内置了react react-router，以约定代替，简化代码 pages/组件即路由
+
+2. layout 业务
